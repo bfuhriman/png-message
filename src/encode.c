1f762ad4198439949cfb0db312829c2017b9ad1e
Braden Fuhriman <111254550+bfuhriman@users.noreply.github.com>
2025-03-07 01:28:54 -0500

    fixed spacing issues

--- src/encode.c
+++ src/encode.c
@@ -36,22 +36,22 @@ int main(int argc, char *argv[]) {
 
 /* Reads bits from str and applies data redundancy, then writes the codeword and its length to img. */
 void encode(Image img, char *str) {
-	if (strlen(str) > maxlen(img)) {
-		printf("Your message is too long. Maximum length for the image provided: %d\n", maxlen(img));
-		return;
-	}
+    if (strlen(str) > maxlen(img)) {
+        printf("Your message is too long. Maximum length for the image provided: %d\n", maxlen(img));
+	return;
+    }
 
     Position pos = {0, 0, 0,};
-	int d = strlen(str) * 8;
-	int p = minRedundancy(d);
-	int len = d + p;
+    int d = strlen(str) * 8;
+    int p = minRedundancy(d);
+    int len = d + p;
     int *bin = getBinary(len);
 
-	write(img, pos, bin, sizeof(int) * 8);
-	pos = increment(img, pos, sizeof(int) * 8);
+    write(img, pos, bin, sizeof(int) * 8);
+    pos = increment(img, pos, sizeof(int) * 8);
     free(bin);
 
-	int *data = getDataBits(str, len);
+    int *data = getDataBits(str, len);
     free(str);
 
     for (int i = 1; i < len; i *= 2) {
@@ -63,13 +63,13 @@ void encode(Image img, char *str) {
         simulateNoise(data, len);
     }
 
-	write(img, pos, data, len);
-	free(data);
+    write(img, pos, data, len);
+    free(data);
 }
 
 /* Allocates memory for the codeword and inserts data bits, leaving power of 2 indices empty. */
 int *getDataBits(char *str, int len) {
-	int *bits = calloc(len, sizeof(int));
+    int *bits = calloc(len, sizeof(int));
     int strIndex = 0;
     int bitIndex = 0;
     char c, bit;
@@ -85,23 +85,23 @@ int *getDataBits(char *str, int len) {
         bits[bitIndex++] = bit;
         strIndex++;
     }
-	return bits;
+    return bits;
 }
 
 /* Writes the data to the least significant bits of each pixel, starting at pos. */
 void write(Image img, Position pos, int* data, int len) {
-	png_bytep row, px;
+    png_bytep row, px;
     for (int i = 0; i < len; i++) {
-		row = img.rows[pos.row];
-		px = &(row[pos.column * 4]);
-		px[pos.channel] = (px[pos.channel] & 0xFE) | data[i];
-		pos = increment(img, pos, 1);
-	}
+	row = img.rows[pos.row];
+	px = &(row[pos.column * 4]);
+	px[pos.channel] = (px[pos.channel] & 0xFE) | data[i];
+	pos = increment(img, pos, 1);
+    }
 }
 
 /* Calculates the maximum number of characters that can be encoded in img. */
 int maxlen(Image img) {
-	int d, max = img.height * img.width * 3;
-	for (d = max; d + minRedundancy(d) + sizeof(int) > max; d--);
-	return d / 8;
+    int d, max = img.height * img.width * 3;
+    for (d = max; d + minRedundancy(d) + sizeof(int) > max; d--);
+    return d / 8;
 }
