6bdb809a71ef8a4d683e3423f57c4f4ab1fc5f16
Braden Fuhriman <111254550+bfuhriman@users.noreply.github.com>
2025-03-07 01:27:08 -0500

    fix spacing issues

--- src/decode.c
+++ src/decode.c
@@ -28,26 +28,26 @@ int main(int argc, char *argv[]) {
 
 /* Decodes message from the least significant bits of each pixel. */
 char *decode(Image img) {
-	Position pos = {0, 0, 0};
-	int len = 0;
-
-	for (int i = 0; i < sizeof(int) * 8; i++) {
-		len = (len << 1) | getBit(img, pos);
-		pos = increment(img, pos, 1);
-	}
+    Position pos = {0, 0, 0};
+    int len = 0;
+	
+    for (int i = 0; i < sizeof(int) * 8; i++) {
+        len = (len << 1) | getBit(img, pos);
+	pos = increment(img, pos, 1);
+    }
 
     if (len == -1) {
         return NULL;
     }
 
     int index = 0;
-	int codeword[len];
+    int codeword[len];
     while (index < len) {
-		codeword[index++] = getBit(img, pos);
-		pos = increment(img, pos, 1);
-	}
+	codeword[index++] = getBit(img, pos);
+	pos = increment(img, pos, 1);
+    }
 
-	int err = findErrors(codeword, len);
+    int err = findErrors(codeword, len);
     if (err > 0) {
         codeword[err] = codeword[err] ? 0 : 1;
         printf("A single-bit error was found and corrected at index %d.\n", err);
@@ -60,30 +60,30 @@ char *decode(Image img) {
 
 /* Parses the codeword and returns the original string. */
 char *getMessage(int *codeword, int len) {
-	int index = 1, bits = 0;
-	char *str = malloc(maxData(len));
-	char c = 0;
+    int index = 1, bits = 0;
+    char *str = malloc(maxData(len));
+    char c = 0;
 
-	while (index < len) {
-		if ((index & (index - 1)) != 0) {
-			c |= codeword[index] << (7 - (bits % 8));
-			bits++;
+    while (index < len) {
+        if ((index & (index - 1)) != 0) {
+	    c |= codeword[index] << (7 - (bits % 8));
+	    bits++;
 
             if (bits % 8 == 0) {
                 str[bits / 8 - 1] = c;
                 c = 0;
             }
-		}
-		index++;
 	}
+	index++;
+    }
     str[bits / 8] = '\0';
     return str;
 }
 
 /* Returns the least significant bit at position pos. */
 int getBit(Image img, Position pos) {
-	png_bytep row, px;
-	row = img.rows[pos.row];
-	px = &(row[pos.column * 4]);
-	return px[pos.channel] & 1;
+    png_bytep row, px;
+    row = img.rows[pos.row];
+    px = &(row[pos.column * 4]);
+    return px[pos.channel] & 1;
 }
